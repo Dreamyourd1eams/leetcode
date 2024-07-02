8b662d30aa1ff7fe12d07a2cdd8aa97cfeaff3b4
Dreamyourd1eams <724104110@qq.com>
2024-07-02 23:12:32 +0800

    Update two_sum.c
    练习熟练c书写

--- two_sum/two_sum.c
+++ two_sum/two_sum.c
@@ -21,17 +21,18 @@ struct hashTable {
 
 struct hashTable* hashtable;
 
-struct hashTable* find(int ikey) {
+struct hashTable* find(int ikey){
     struct hashTable* tmp;
     HASH_FIND_INT(hashtable, &ikey, tmp);
     return tmp;
 }
 
-void insert(int ikey, int ival) {
+void insert(int ikey, int ival){
     struct hashTable* it = find(ikey);
     if(it == NULL) {
         struct hashTable* tmp = malloc(sizeof(struct hashTable));
-        tmp->key = ikey, tmp->val = ival;
+        tmp->key = ikey;
+        tmp->val = ival;
         HASH_ADD_INT(hashtable, key, tmp);
     } else {
         it->val = ival;
@@ -41,16 +42,15 @@ void insert(int ikey, int ival) {
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     hashtable = NULL;
     for (int i = 0; i < numsSize; i++) {
-        struct hashTable* it = find(target - nums[i]);
-        if (it != NULL) {
+        if(find(target - nums[i])){
             int* ret = malloc(sizeof(int) * 2);
-            ret[0] = it->val, ret[1] = i;
-            *returnSize = 2;
+            ret[0] = i;
+            ret[1] = find(target - nums[i])->val;
             return ret;
+        } else {
+            insert(nums[i], i);
         }
-        insert(nums[i], i);
     }
     *returnSize = 0;
     return NULL;
 }
-
